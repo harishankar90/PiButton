c7656dd777c4874a359d52c91e90288937874fcc
Harishankar <harishankar714@gmail.com>
2019-05-30 21:22:55 +0530

    fully finished the app

--- app/src/main/java/com/hvdcreations/pibutton/LandingActivity.java
+++ app/src/main/java/com/hvdcreations/pibutton/LandingActivity.java
@@ -1,6 +1,7 @@
 package com.hvdcreations.pibutton;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,29 +9,71 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 public class LandingActivity extends AppCompatActivity {
 
+    ConstraintLayout btn,gif;
+
     ImageView status;
     TextView txt;
+    GifDrawable piconngif;
+    private int SLEEP_TIMER = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_landing);
+
+        btn = findViewById(R.id.btnlayout);
+        gif = findViewById(R.id.giflayout);
 
         txt = findViewById(R.id.textViewBtn);
         status = findViewById(R.id.imBtn);
+
+        try {
+            piconngif = new GifDrawable( getResources(), R.drawable.piconn);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            //Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
         status.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LandingActivity.this, MainActivity.class);
-                startActivity(intent);
+                btn.setVisibility(View.INVISIBLE);
+                gif.setVisibility(View.VISIBLE);
 
+                LogoLauncher logoLauncher = new LogoLauncher();
+                logoLauncher.start();
 
             }
         });
 
+    }
+
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(1000 * SLEEP_TIMER);
+                final MediaController mc = new MediaController(LandingActivity.this);
+                mc.setMediaPlayer(piconngif);
+                mc.show();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(LandingActivity.this, MainActivity.class);
+            startActivity(intent);
+            LandingActivity.this.finish();
+        }
     }
 }
--- app/src/main/java/com/hvdcreations/pibutton/MainActivity.java
+++ app/src/main/java/com/hvdcreations/pibutton/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.txt_info);
